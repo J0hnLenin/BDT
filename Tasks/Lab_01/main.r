e371c224dd234bae248d045a2dbdeb7e87f6e68f
John Lenin <ziziz123ziziz@gmail.com>
2023-09-18 16:51:15 +0500

    Update main.r

--- Tasks/Lab_01/main.r
+++ Tasks/Lab_01/main.r
@@ -1,12 +1,29 @@
-task_1 <- function(){
-    width <- 2
-    len <- width + 7
-    per <- len + len + width + width 
-    print(per)
+task_1 <- function() {
+  width <- 2
+  len <- width + 7
+  per <- len + len + width + width
+  return(per)
 }
 
-task_4 <- function(){
-    print(c(-3, -2, -1))
+task_4 <- function() {
+  return(c(-3, -2, -1))
 }
 
-task_4()
+task_6 <- function() {
+  month <- c("January", "February", "March", "April", "May", "June", "JUly", "August", "September", "October", "November", "December")
+  return(month)
+}
+
+task_7 <- function() {
+  month <- task_6()
+  return(c(month[0:3], month[6:9]))
+}
+
+task_9 <- function(){
+  month <- task_6()
+  calendar <- matrix(month, nrow = 3, ncol = 4)  
+  return(calendar)
+
+}
+
+print(task_9())
