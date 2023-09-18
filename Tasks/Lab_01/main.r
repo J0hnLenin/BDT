4e3b3be62e332897b0855e5e6394e4f217e14c52
Iversy <iversy49@ya.ru>
2023-09-18 16:47:57 +0500

    0-8

--- Tasks/Lab_01/main.r
+++ Tasks/Lab_01/main.r
@@ -1,2 +1,23 @@
-x <- 5
-print(x)
+
+task_2 <- function() {
+    side_1 <- 3
+    side_2 <- 5
+    len <- side_1 * side_2
+    return(len)
+}
+
+task_5 <- function() {
+    season <- c("winter", "winter", "spring", "spring", "spring", "summer", "summer", "summer", "autumn", "autumn", "autumn", "winter")
+    return(season)
+}
+
+
+task_8 <- function() {
+    powers_of_two <- c()
+    for(i in 1:5) {
+        powers_of_two <- c(powers_of_two, 2^i)
+    }
+    return(powers_of_two)
+}
+
+task_8()
